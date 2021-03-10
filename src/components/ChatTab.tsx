1ea36b6bd32c82bd1ba17ab3a08a1b3fb682c325
Larry Baer <lbaer724@gmail.com>
2021-03-09 21:25:25 -0500

    Updated side bar ui

--- src/components/ChatTab.tsx
+++ src/components/ChatTab.tsx
@@ -1,34 +1,49 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 // Import icons
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 const useStyles = makeStyles({
-    chat__tab:{
-      backgroundColor:"lightblue",
-      width:"100%",
-      height:"60px",
-    },
-    chat__tab__img:{
-
-    },
-    chat__tab__username:{},
-    message__preview:{},
+  chat__tab: {
+    display: "flex",
+    width: "100%",
+    height: "60px",
+  },
+  chat__tab__img__container: {},
+  chat__tab__img: {
+    color: "green",
+    fontSize: 40,
+    position: "relative",
+    top: 9,
+  },
+  chat__tab__username: {
+    fontWeight: "bold",
+  },
+  message__preview: {},
+  chat__tab__info: {
+    fontSize: 14,
+    paddingLeft: "10px",
+    position: "relative",
+    top: 8,
+  },
 });
 
 function ChatTab() {
   const classes = useStyles();
   return (
     <div className={classes.chat__tab}>
-      <div className={classes.chat__tab__img}>
-        <AccountCircleIcon/>
+      <div className={classes.chat__tab__img__container}>
+        <AccountCircleIcon
+          fontSize="large"
+          className={classes.chat__tab__img}
+        />
       </div>
-      <div className={classes.chat__tab__username}>
-        John Doe
-      </div>
-      <div className={classes.message__preview}>
-        Hey man, this is a test...
+      <div className={classes.chat__tab__info}>
+        <div className={classes.chat__tab__username}>John Doe</div>
+        <div className={classes.message__preview}>
+          Hey man, this is here to test the UI...
+        </div>
       </div>
     </div>
   );
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -22,85 +22,118 @@ const useStyles = makeStyles({
   sidebar: {
     width: "330px",
     height: "100vh",
-    paddingLeft:"40px",
-    paddingRight:"40px",
+    paddingLeft: "40px",
+    paddingRight: "40px",
     backgroundColor: "white",
   },
   sidebar__nav: {
     width: "100%",
     height: "10%",
-    // backgroundColor: "lightgreen",
     display: "flex",
     justifyContent: "space-between",
   },
-  chats__selection: {
-    // backgroundColor: "lightyellow",
+  chats__selection: {},
+  chats__selection__header__container: {
+    display: "flex",
+    justifyContent: "space-between",
   },
   chats__selection__header: {
-    display: "flex",
-    justifyContent: "space-between",
+    paddingLeft: "15px",
   },
   chats__selection__options: {
     display: "flex",
     justifyContent: "space-between",
+    paddingLeft: "18px",
+    paddingRight: "14px",
   },
-  search__chats: {
-    paddingTop:"5vh",
+  search__chats__container: {
+    paddingTop: "5vh",
     display: "flex",
     justifyContent: "space-between",
+    paddingLeft: "16px",
   },
-  chats: {},
+  search__chats: {
+    color: "red",
+  },
+  chats: {
+    paddingTop: "5vh",
+  },
+  account__icon: {
+    fontSize: 30,
+  },
+  contacts__icon: {
+    fontSize: 30,
+  },
+  settings__icon: {
+    fontSize: 30,
+  },
+  exit__icon: {
+    fontSize: 30,
+  },
+  add__chat__icon: {
+    fontSize: 30,
+  },
 });
 
 function SideBar() {
   const classes = useStyles();
   return (
     <div className={classes.sidebar}>
-        <div className={classes.sidebar__nav}>
+      <div className={classes.sidebar__nav}>
+        <IconButton>
+          <AccountCircleIcon className={classes.account__icon} />
+        </IconButton>
+        <IconButton>
+          <PeopleIcon className={classes.contacts__icon} />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon className={classes.settings__icon} />
+        </IconButton>
+        <IconButton>
+          <ExitToAppIcon className={classes.exit__icon} />
+        </IconButton>
+      </div>
+      <div className={classes.chats__selection}>
+        <div className={classes.chats__selection__header__container}>
+          <h1 className={classes.chats__selection__header}>Chats</h1>
           <IconButton>
-            <AccountCircleIcon />
-          </IconButton>
-          <IconButton>
-            <PeopleIcon />
-          </IconButton>
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
-          <IconButton>
-            <ExitToAppIcon />
+            <AddCircleIcon className={classes.add__chat__icon} />
           </IconButton>
         </div>
-        <div className={classes.chats__selection}>
-          <div className={classes.chats__selection__header}>
-            <h1>Chats</h1>
-            <IconButton>
-              <AddCircleIcon />
-            </IconButton>
-          </div>
-          <div className={classes.chats__selection__options}>
-            <Typography>Direct</Typography>
-            <Typography>Group</Typography>
-            <Typography>Public</Typography>
-          </div>
+        <div className={classes.chats__selection__options}>
+          <Typography>Direct</Typography>
+          <Typography>Group</Typography>
+          <Typography>Public</Typography>
         </div>
-        <div className={classes.search__chats}>
-          <form>
-            <TextField
-              id="standard-basic"
-              label="Search"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </form>
-        </div>
-        <div className={classes.chats}>
-          <ChatTab />
-        </div>
+      </div>
+      <div className={classes.search__chats__container}>
+        <form>
+          <TextField
+            className={classes.search__chats}
+            id="standard-basic"
+            label="Search"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </form>
+      </div>
+      <div className={classes.chats}>
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+        <ChatTab />
+      </div>
     </div>
   );
 }
